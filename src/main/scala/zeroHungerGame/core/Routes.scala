91965d95643379ec6bbd24bac62b7a03dc43c022
Justin-yww <justinyong2003@yahoo.com>
2025-08-15 23:58:04 +0800

    📦 NEW: Updated Routes.scala file

--- src/main/scala/zeroHungerGame/core/Routes.scala
+++ src/main/scala/zeroHungerGame/core/Routes.scala
@@ -1,22 +1,57 @@
 package zeroHungerGame.core
 
-sealed trait Screen {
-      def fxmlPath: Option[String] 
-      def title: String
-}
-
-object Routes { 
-      // NOTE: Additional Screens can be added here
-      // [1] Main Menu 
-      case object Menu extends Screen {
-            override def fxmlPath: Option[String] = Some("/zeroHungerGame/view/Menu.fxml")
-            override def title: String = "Main Menu"
+object Routes {
+      // NOTE: Trait will be used for all screens 
+      sealed trait Screen {
+            def fxmlPath: Option[String] = None
+            def id: String // Allow better control over screen identification and sequencing
       }
 
-      // [2] Educational Info Display Screen
-      case class EduVillage(currentPage: Int = 0) extends Screen { 
-            override def fxmlPath: Option[String] = Some("/zeroHungerGame/view/Education.fxml")
-            override def title: String = "Sproutfiled - The Nutrition within the rural context"
+      // [1] Main Menu Screen
+      case object Menu extends Screen {
+            override def fxmlPath: Option[String] = Some("view/Menu.fxml")
+            override def id: String = "menu"
+      }
+
+      // [2] Village Mode - Education Section 
+      final case class VillageEduation(pageNumber: Int) extends Screen{
+            override val fxmlPath  = Some("view/Education.fxml")
+            override val id        = s"village-edu-$pageNumber"
+
+            // This will be the total number of pages for the educational section [Village]
+            private val totalPages  = 5
+            def isLastPage: Boolean = pageNumber >= totalPages
+            def nextPage: Int       = if (isLastPage) pageNumber else pageNumber + 1
+            def prevPage: Int       = if (pageNumber <= 1) pageNumber else pageNumber - 1
+      }
+
+
+      // [3] Urban Mode - Eduaciton Section 
+      final case class UrbanEducation(pageNumber: Int) extends Screen {
+            override val fxmlPath  = Some("view/Education.fxml")
+            override val id        = s"urban-edu-$pageNumber"
+
+            // This will be the total number of pages for the educational section [Urban]
+            private val totalPages  = 5
+            def isLastPage: Boolean = pageNumber >= totalPages
+            def nextPage: Int       = if (isLastPage) pageNumber else pageNumber + 1
+            def prevPage: Int       = if (pageNumber <= 1) pageNumber else pageNumber - 1
+      }
+
+      // [4] Village Mode - Game Section
+      case object GameVillage extends Screen { 
+            override val id = "game-village"
+      }
+
+      // [5] Urban Mode - Game Section
+      case object GameUrban extends Screen {
+            override val id = "game-urban"
+      }
+
+      // [6] Result 
+      case object Result extends Screen {
+            override val fxmlPath  = Some("view/Result.fxml")
+            override def id        = "result"
       }
 
 }
